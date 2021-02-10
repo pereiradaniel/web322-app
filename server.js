6ab70aaf04d4c6723ade6fff3b1fda3ed9169d52
pereiradaniel <danielpaulpereira@protonmail.com>
2021-02-09 19:40:21 -0500

    Add body-parser, clean up comments.

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var multer = require("multer");
 const fs = require('fs');
+const bodyParser = require("body-parser");
 var app = express();
 var path = require("path");
 var dataService = require("./data-service.js");
@@ -23,7 +24,6 @@ function onHttpStart() {
 };
 
 // Multer setup
-///////////////
 const storage = multer.diskStorage({
     destination: "./public/images/uploaded",
     filename: function (req, file, cb) {
@@ -32,62 +32,67 @@ const storage = multer.diskStorage({
 });
 const upload = multer({storage: storage});  // use diskStorage function for naming files instead of the default.
 
-// NOTE: for your server to correctly return the "/css/site.css" file, the "static" middleware must be used:  in your server.js file, add the line: app.use(express.static('public')); before your "routes"
+// Set middleware for 'urlencoded' form data (normal HTTP post data)
+app.use(bodyParser.urlencoded({extended: true}));
+
+// NOTE: for server to correctly return the "/css/site.css" file, "static" middleware must be used before "routes"
 app.use(express.static('public'));
 
-// The route "/" must return the home.html file from the views folder.
+// HOME
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname,"/views/home.html"));
 });
 
-// The route "/about" must return the about.html file from the views folder.
+// ABOUT
 app.get("/about", (req,res) => {
     res.sendFile(path.join(__dirname,"/views/about.html"));
 });
 
-// Assignment 3, Step 2: Add new routes.
+// ADD EMPLOYEES
 app.get("/employees/add", (req,res) => {
     res.sendFile(path.join(__dirname, "/views/addEmployee.html"));
 });
 
-app.get("/images/add", (req,res) => {
-    res.sendFile(path.join(__dirname, "/views/addImage.html"));
-});
-
-app.post("/images/add", upload.single("imageFile"), (req, res) => {
-    res.redirect("/images");
-});
-
+// IMAGES
 app.get("/images", (req, res) => {
     fs.readdir(path.join(__dirname, "/public/images/uploaded"), function(err, items) {
         res.json(items);
     });
 });
 
-// Routes
+// ADD IMAGES
+app.get("/images/add", (req,res) => {
+    res.sendFile(path.join(__dirname, "/views/addImage.html"));
+});
+app.post("/images/add", upload.single("imageFile"), (req, res) => {
+    res.redirect("/images");
+});
+
+// MANAGERS
 app.get("/managers", (req,res) => {
     dataService.getManagers()
         .then((data) => {res.json(data)})
         .catch((err) => {res.json(err)})
 });
 
+// EMPLOYEES
 app.get("/employees", (req,res) => {
     dataService.getAllEmployees()
         .then((data) => {res.json(data)})
         .catch((err) => {res.json(err)})
 });
 
+// DEPARTMENTS
 app.get("/departments", (req,res) => {
     dataService.getDepartments()
         .then((data) => {res.json(data)})
         .catch((err) => {res.json(err)})
 });
 
+// Catch 404's
 app.get("*", (req,res) => {
     res.sendFile(path.join(__dirname,"/views/404.html"),404);
 });
-
-
 
 // Only call app.listen() if our call to the initialize() method is successful
 dataService.initialize()
